a784668bf0236989f5c0f283ade3a2141960d4af
apsegian <apsegian@gmail.com>
2013-10-04 13:36:02 +0300

    added constructor in parseexception with throwable

--- agro-domain/src/main/java/com/agroknow/domain/parser/AbstractParser.java
+++ agro-domain/src/main/java/com/agroknow/domain/parser/AbstractParser.java
@@ -6,7 +6,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import javax.annotation.Resource;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
@@ -23,14 +22,13 @@ public abstract class AbstractParser implements Parser {
 
     protected abstract SimpleMetadata loadInternal(FileInputStream fileReader) throws IOException;
 
-    @Override
     public SimpleMetadata load(String filePath) throws ParserException {
         FileInputStream fileReader = null;
         try {
             fileReader = FileUtils.openInputStream(new File(filePath));
             return loadInternal(fileReader);
         } catch (IOException e) {
-            throw new ParserException("Error loading file " + filePath + " - " + e.getMessage());
+            throw new ParserException("Error loading file " + filePath, e);
         } finally {
             IOUtils.closeQuietly(fileReader);
         }
--- agro-domain/src/main/java/com/agroknow/domain/parser/Parser.java
+++ agro-domain/src/main/java/com/agroknow/domain/parser/Parser.java
@@ -1,14 +1,11 @@
-
 package com.agroknow.domain.parser;
 
 import com.agroknow.domain.SimpleMetadata;
 
 /**
- *
  * @author aggelos
  */
 public interface Parser {
 
     SimpleMetadata load(String filePath) throws ParserException;
-
 }

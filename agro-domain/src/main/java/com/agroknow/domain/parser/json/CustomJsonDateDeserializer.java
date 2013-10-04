b56c117de43ed3c957bb84973e883ee61ecf0262
apsegian <apsegian@gmail.com>
2013-10-04 14:07:29 +0300

    added one more date format in dateDeserializer

--- agro-domain/src/main/java/com/agroknow/domain/parser/json/CustomJsonDateDeserializer.java
+++ agro-domain/src/main/java/com/agroknow/domain/parser/json/CustomJsonDateDeserializer.java
@@ -18,7 +18,7 @@ public class CustomJsonDateDeserializer extends JsonDeserializer<Date> {
     public Date deserialize(JsonParser parser, DeserializationContext context) throws IOException, JsonProcessingException {
         String date = parser.getText();
         try {
-            return DateUtils.parseDate(date, new String[]{"yyyy-MM-dd", "yyyy-MM", "yyyy-MM-dd'T'HH:mm:ss.SS'Z'"});
+            return DateUtils.parseDate(date, new String[]{"yyyy-MM-dd", "yyyy-MM", "yyyy", "yyyy-MM-dd'T'HH:mm:ss.SS'Z'"});
         } catch (ParseException e) {
             throw new JsonParseException("Error parsing date " + date, parser.getCurrentLocation(), e);
         }
